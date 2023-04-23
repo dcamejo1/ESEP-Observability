ab92dbe4d3194ed891e120bc6da5c5356fa234a4
danielcamejo <dcamejo1@ufl.edu>
2023-04-23 12:14:31 +0200

    Jeager working and todo appearing within jeager after running the commands specified in the instructions

---
+++ instrumentation.js
@@ -0,0 +1,21 @@
+const opentelemetry = require("@opentelemetry/sdk-node");
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-proto");
+const {
+  OTLPMetricExporter
+} = require("@opentelemetry/exporter-metrics-otlp-proto");
+
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    url: "http://localhost:14268/v1/traces",
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+sdk.start();
